aeef084dbc76fa7d493490971ee512658d6a1246
Дима Сахно <dimasahno8@gmail.com>
2021-05-18 18:54:32 +0300

    task1

---
+++ calculator/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication19
+{
+    public partial class Form1 : Form
+    {
+        public string D;
+        public string N1;
+        public bool N2;
+        public Form1()
+        {
+            N2 = false;
+            InitializeComponent();
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            double dn, res;
+
+            dn = Convert.ToDouble(textBox1.Text);
+            res = -dn;
+            textBox1.Text = res.ToString();
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            if (N2)
+            {
+                N2 = false;
+                textBox1.Text = "0";
+            }
+            Button B = (Button)sender;
+            if(textBox1.Text=="0")
+                textBox1.Text = B.Text;
+            else
+                textBox1.Text = textBox1.Text + B.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            D = B.Text;
+            N1 = textBox1.Text;
+            N2 = true;
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            double dN1, dN2,res;
+            res = 0;
+            dN1 = Convert.ToDouble(N1);
+            dN2 = Convert.ToDouble(textBox1.Text);
+            if (D == "+")
+            {
+                res = dN1 + dN2;
+            }
+            if (D == "-")
+            {
+                res = dN1 - dN2;
+            }
+            if (D == "X")
+            {
+                res = dN1 * dN2;
+            }
+            if (D == "/")
+            {
+                res = dN1 / dN2;
+            }
+            if (D == "%")
+            {
+                res = dN1 * dN2 /100;
+            }
+            D = "=";
+            N2 = true;
+            textBox1.Text = res.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double dn,res;
+
+            dn = Convert.ToDouble(textBox1.Text);
+            res = Math.Sqrt(dn);
+            textBox1.Text = res.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double dn, res;
+
+            dn = Convert.ToDouble(textBox1.Text);
+            res = Math.Pow(dn,2);
+            textBox1.Text = res.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double dn, res;
+
+            dn = Convert.ToDouble(textBox1.Text);
+            res = 1/dn;
+            textBox1.Text = res.ToString();
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            if(!textBox1.Text.Contains(","))
+                textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text=textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            if (textBox1.Text == "")
+                textBox1.Text = "0";
+        }
+    }
+}
